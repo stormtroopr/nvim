1b5a95412bc69899c1e8edeaab18dba6389f7511
unknown <marcus.lecount@me.com>
2025-05-05 14:35:34 +0800

    add configs for harpoon and a colour scheme

--- init.lua
+++ init.lua
@@ -238,6 +238,8 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  { import = "custom.plugins" },
+
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
