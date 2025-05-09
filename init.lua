f1cc27373bd25df5aa7f5f221ed449ee414c50b7
marcus <marcus.lecount@me.com>
2025-05-09 11:28:38 +0800

    some colorscheme changes

--- init.lua
+++ init.lua
@@ -876,6 +876,7 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
+    lazy = true,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
