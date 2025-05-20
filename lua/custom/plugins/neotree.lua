4054c6edfa724b367ffbc3cbb7b752ea77b86975
marcus <marcus.lecount@me.com>
2025-05-20 10:39:15 +0800

    add neo tree to the custom config

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,13 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup()
+    vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<CR>', { desc = 'Toggle Neo-tree' })
+  end,
+}
