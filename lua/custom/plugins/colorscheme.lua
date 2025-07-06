79425d6e22c40047abda4c7697f75ad14715847e
marcus <marcus.lecount@me.com>
2025-07-06 13:16:01 +0800

    some new colour schemes

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,6 +1,47 @@
+-- return {
+--   'tanvirtin/monokai.nvim',
+--   config = function()
+--     vim.cmd 'colorscheme monokai'
+--   end,
+-- }
+
 return {
-  'tanvirtin/monokai.nvim',
-  config = function()
-    vim.cmd 'colorscheme monokai'
-  end,
+  {
+    'cpplain/flexoki.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd 'colorscheme flexoki'
+    end,
+  },
 }
+
+-- return {
+--   'catppuccin/nvim',
+--   name = 'catppuccin',
+--   priority = 1000,
+--   config = function()
+--     vim.cmd 'colorscheme catppuccin-mocha'
+--   end,
+-- }
+
+-- return {
+--   'folke/tokyonight.nvim',
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.cmd 'colorscheme tokyonight'
+--   end,
+-- }
+
+-- return {
+--   {
+--     'jacoborus/tender.vim',
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--       vim.opt.termguicolors = true
+--       vim.cmd 'colorscheme tender'
+--     end,
+--   },
+-- }
