44c834f72f53c38d62dc03d2f14abb466edb580a
marcus <marcus.lecount@me.com>
2025-08-04 07:54:26 +0800

    increase timeout of formatter

--- lua/custom/plugins/formatters.lua
+++ lua/custom/plugins/formatters.lua
@@ -5,7 +5,7 @@ return {
     format_on_save = function(bufnr)
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
       if ok and stats and stats.size < 100 * 1024 then
-        return { timeout_ms = 500, lsp_fallback = true }
+        return { timeout_ms = 10000, lsp_fallback = true }
       end
     end,
 
